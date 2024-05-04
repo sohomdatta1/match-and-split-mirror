7f1008453142f6486d7b0f91030088d4ce57d78b
Sohom <sohomdatta1+git@gmail.com>
2024-05-04 18:29:51 -0400

    add disclaimer in edit summary

--- pywikibot_utils.py
+++ pywikibot_utils.py
@@ -2,5 +2,5 @@ import pywikibot as pwb
 
 def safe_put(page: pwb.Page, text: str, summary: str):
     page.text = text
-    page.save(summary, botflag=True)
+    page.save(summary + ' (EXPERIMENTAL MATCH AND SPLIT EDIT, REVERT IF INCORRECT)', botflag=True)
 
