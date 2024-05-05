4e23284c33a5ccfa6e03b34bb621a8d5b7e82548
Sohom <sohomdatta1+git@gmail.com>
2024-05-04 20:50:56 -0400

    fixes

--- pywikibot_utils.py
+++ pywikibot_utils.py
@@ -3,4 +3,3 @@ import pywikibot as pwb
 def safe_put(page: pwb.Page, text: str, summary: str):
     page.text = text
     page.save(summary + ' (EXPERIMENTAL MATCH AND SPLIT EDIT, REVERT IF INCORRECT)', botflag=True)
-
