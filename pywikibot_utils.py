5f1f595c6a02916a3de47e8cb71fd12f066ae045
Sohom <sohomdatta1+git@gmail.com>
2024-05-04 22:03:45 -0400

    fix issues with weird js behaviour

--- pywikibot_utils.py
+++ pywikibot_utils.py
@@ -1,5 +1,8 @@
 import pywikibot as pwb
 
 def safe_put(page: pwb.Page, text: str, summary: str):
+    print('TXT:', text)
+    print('PAGE:', page.title())
+    print('SUMMARY:', summary)
     page.text = text
     page.save(summary + ' (EXPERIMENTAL MATCH AND SPLIT EDIT, REVERT IF INCORRECT)', botflag=True)
