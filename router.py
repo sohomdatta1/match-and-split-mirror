539de7ced8040b8f782eaf241c737663f38e2ec9
Sohom <sohomdatta1+git@gmail.com>
2024-05-04 21:13:56 -0400

    add ui

--- router.py
+++ router.py
@@ -1,30 +1,30 @@
-from flask import request, jsonify
+from flask import request, send_file
 from app import flask_app as app
 from matchandsplit import match, split
 
-@app.route('/match', methods=['POST'])
+@app.route('/match', methods=['POST', 'GET'])
 def match_route():
+    if request.method == 'GET':
+        return send_file('match.html')
     lang = request.form['lang']
     title = request.form['title']
     username = request.form['username']
     match.delay(lang, title, username)
-    r = jsonify({'status': 'recieved'})
-    r.headers['Access-Control-Allow-Origin'] = '*'
-    return r
+    return {'status': 'recieved'}
 
-@app.route('/split', methods=['POST'])
+@app.route('/split', methods=['POST', 'GET'])
 def split_route():
+    if request.method == 'GET':
+        return send_file('split.html')
     lang = request.form['lang']
     title = request.form['title']
     username = request.form['username']
     split.delay(lang, title, username)
-    r = jsonify({'status': 'recieved'})
-    r.headers['Access-Control-Allow-Origin'] = '*'
-    return r
+    return {'status': 'recieved'}
 
 @app.route('/')
 def index():
-    return """This is experimental API for match and split. /match and /split are the two POST endpoints."""
+    return """This is experimental API for match and split. <a href="/match">/match</a> and <a href="/split">/split</a> are the two endpoints.<br><b>You are responsible for verifying the results.</b>"""
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=1238)
