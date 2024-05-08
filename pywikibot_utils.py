a0774a89bca7cadf205e96251407318eedd80b51
Sohom <sohomdatta1+git@gmail.com>
2024-05-08 07:57:02 -0400

    Fix split endpoint wiring

--- pywikibot_utils.py
+++ pywikibot_utils.py
@@ -1,5 +1,8 @@
 import pywikibot as pwb
 
 def safe_put(page: pwb.Page, text: str, summary: str):
+    print('TITLE:', page.title())
+    print('TEXT:', text)
+    print('SUMMARY:', summary)
     page.text = text
     page.save(summary + ' (EXPERIMENTAL MATCH AND SPLIT EDIT, REVERT IF INCORRECT)', botflag=True)
