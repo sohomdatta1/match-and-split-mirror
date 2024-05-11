67711848d58564a69cd8dded2ee126300c1b2c05
Sohom <sohomdatta1+git@gmail.com>
2024-05-11 10:15:31 -0400

    Gaurd against multiple values

--- pywikibot_utils.py
+++ pywikibot_utils.py
@@ -5,4 +5,4 @@ def safe_put(page: pwb.Page, text: str, summary: str):
     print('TEXT:', text)
     print('SUMMARY:', summary)
     page.text = text
-    page.save(summary + ' (EXPERIMENTAL MATCH AND SPLIT EDIT, REVERT IF INCORRECT)', botflag=True)
+    page.save(summary=str( summary ) + ' (EXPERIMENTAL MATCH AND SPLIT EDIT, REVERT IF INCORRECT)', botflag=True)
