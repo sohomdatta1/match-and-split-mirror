60e8debd4af31c160a1adeb320cd7892cefdd846
Sohom <sohomdatta1+git@gmail.com>
2024-06-10 00:04:26 -0400

    Also vitally important to screw up your envvars

--- cnf.py
+++ cnf.py
@@ -15,8 +15,8 @@ if not os.environ.get( 'TOOLFORGE' ):
     password = cnf['client']['password']
 else:
     remote = 'tools.db.svc.wikimedia.cloud'
-    user = os.environ.get( 'TOOL_DATABASE_USER' )
-    password = os.environ.get( 'TOOL_DATABASE_PASSWORD' )
+    user = os.environ.get( 'TOOL_TOOLSDB_USER' )
+    password = os.environ.get( 'TOOL_TOOLSDB_PASSWORD' )
 
 
 config = {
