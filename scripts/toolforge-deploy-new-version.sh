6b5fe3623f737a944ccf4a2ae3f8bec2ca0b04d9
Sohom <sohomdatta1+git@gmail.com>
2024-06-13 13:57:58 -0400

    log the fact that the script was run

--- scripts/toolforge-deploy-new-version.sh
+++ scripts/toolforge-deploy-new-version.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -ex
+dologmsg "./matchandsplit/scripts/toolforge-deploy-new-version.sh"
 toolforge build start https://gitlab.wikimedia.org/toolforge-repos/matchandsplit.git
 cd matchandsplit
 git pull
