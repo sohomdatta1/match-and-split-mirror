204648e415c2cb4db6e1c023207d2a67044940a5
Sohom <sohomdatta1+git@gmail.com>
2024-07-14 23:35:49 +0530

    Reorder cd and git pull to prevent accidental breakages

--- scripts/toolforge-deploy-new-version.sh
+++ scripts/toolforge-deploy-new-version.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -ex
-dologmsg "./matchandsplit/scripts/toolforge-deploy-new-version.sh"
-toolforge build start https://gitlab.wikimedia.org/toolforge-repos/matchandsplit.git
 cd matchandsplit
 git pull
+dologmsg "./matchandsplit/scripts/toolforge-deploy-new-version.sh"
+toolforge build start https://gitlab.wikimedia.org/toolforge-repos/matchandsplit.git
 toolforge webservice restart
 toolforge jobs delete mascelery
 toolforge jobs load jobs.yaml
